0ce8ab916776c2203cc53cf1396e9cbab11e825e
cdszoke-coder <cdszoke@gmail.com>
2026-06-03 17:30:38 -0400

    Update server.js

--- src/server.js
+++ src/server.js
@@ -16,8 +16,7 @@ initDatabase();
 
 const publicRoutes = require('./routes/public');
 const adminRoutes = require('./routes/admin');
-const welcomeRoutes = require('./routes/welcome');
-app.use('/api', welcomeRoutes);
+
 
 // Accept both export styles:
 //   module.exports = router                  (function)
@@ -46,7 +45,8 @@ const artistsRoutes        = safeRequireRouter('./routes/artists');
 const youtubeRoutes        = safeRequireRouter('./routes/youtube');
 const artistYoutubeRoutes  = safeRequireRouter('./routes/artists_youtube');
 const youtubeLinkRoutes    = safeRequireRouter('./routes/youtube_link');
-
+const welcomeRoutes = require('./routes/welcome');
+app.use('/api', welcomeRoutes);
 const app = express();
 app.use(cors());
 app.use(express.json({ limit: '1mb' }));

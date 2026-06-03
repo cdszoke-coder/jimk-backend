84c1c3284b83dfd8e568398d1ac1e8dc60b9bb0c
cdszoke-coder <cdszoke@gmail.com>
2026-06-03 17:37:25 -0400

    Update server.js

--- src/server.js
+++ src/server.js
@@ -45,13 +45,13 @@ const artistsRoutes        = safeRequireRouter('./routes/artists');
 const youtubeRoutes        = safeRequireRouter('./routes/youtube');
 const artistYoutubeRoutes  = safeRequireRouter('./routes/artists_youtube');
 const youtubeLinkRoutes    = safeRequireRouter('./routes/youtube_link');
-const welcomeRoutes = require('./routes/welcome');
-app.use('/api', welcomeRoutes);
+const welcomeRoutes        = require('./routes/welcome');
+
 const app = express();
 app.use(cors());
 app.use(express.json({ limit: '1mb' }));
 app.use(express.urlencoded({ extended: true }));
-
+app.use('/api', welcomeRoutes);
 app.get('/healthz', (req, res) => res.json({ ok: true, service: 'jimk-2qr-backend' }));
 
 if (corePublic)          app.use('/api/public', corePublic);

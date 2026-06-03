977f66c45297846dca4a4ea63232ca5088e5a18a
cdszoke-coder <cdszoke@gmail.com>
2026-06-03 02:34:01 -0400

    Update server.js

--- src/server.js
+++ src/server.js
@@ -16,7 +16,8 @@ app.use(express.json({ limit: '1mb' }));
 app.use(express.urlencoded({ extended: true }));
 const artistYoutubeRoutes = require('./routes/artists_youtube');
 app.use('/api', artistYoutubeRoutes);
-
+const artistYoutubeRoutes = require('./routes/artists_youtube');
+app.use('/api', artistYoutubeRoutes);
 
 app.get('/healthz', (req, res) => {
   res.status(200).json({ ok: true, service: 'jimk-2qr-backend' });
